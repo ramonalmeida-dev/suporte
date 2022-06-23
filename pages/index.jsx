e75e3a075175046688460f732fce961c929ba229
Ramonalmeida-dev <ramonrocha1998@hotmail.com>
2022-06-23 11:55:47 -0300

    Adicionado tela de login

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,80 +1,70 @@
-import { getData } from './api/getData';
-import DefaultLayout from '../components/Layout';
+import React, { Component } from "react";
 
-
-const Index = ({ downloads = [] }) => {
-
-  return(
-    <DefaultLayout>
-    <div className="overflow-x-auto">
-        <div className=" bg-gray-100 flex items-center justify-center font-sans overflow-hidde">
-            <div className="w-full lg:w-5/6">
-                <div className="bg-white shadow-md rounded my-6">
-               
-                    <table className="w-full table-auto" >
-                        <thead>
-                            <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                <th className="py-3 px-6 text-left">Nome</th>
-                                <th className="py-3 px-3 text-left">Versão</th>
-                                <th className="py-3 px-6 text-center">Autor</th>
-                                <th className="py-3 px-6 text-center">Categoria</th>
-                                <th className="py-3 px-6 text-center">Ação</th>
-                            </tr>
-                        </thead>
-                        {downloads.map((download, index) => (
-                        <tbody className="text-gray-600 text-sm font-light" key={index}>
-                            <tr className="border-b border-gray-200 hover:bg-gray-100">
-                                <td className="py-3 px-6 text-left whitespace-nowrap">
-                                    <div className="flex items-center">
-                                        <div className="mr-2">
-                                        <img src={download.data.imagem.url} className="w-10 h-10"/>
-                                        </div>
-                                        <span className="font-medium">{download.data.titulo[0].text}</span>
-                                    </div>
-                                </td>
-                                <td className="py-3 px-6 text-center whitespace-nowrap">
-                                    <div className="flex items-center">
-                                        <span className="font-sm">{download.data.versao[0].text}</span>
-                                    </div>
-                                </td>
-                                <td className="py-3 px-6 text-center">
-                                    <div className="flex items-center justify-center">
-                                    {download.data.autor}
-                                    </div>
-                                </td>
-                                <td className="py-3 px-6 text-center">
-                                    <span className="bg-purple-200 text-blue-600 py-1 px-3 rounded-full text-xs">{download.data.categoria}</span>
-                                </td>
-                                <td className="py-3 px-6 text-center">
-                                    <div className="flex item-center justify-center">
-                                        
-                                        <a  className="flex-no-shrink bg-blue-500 px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-blue-500 text-white rounded-full" href={download.data.link.url}>Download</a>
-                                        
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                        ))}
-                    </table>
-                     
-                </div>
-                
-            </div>
-        </div>
-    </div>
+class Home extends Component { 
    
-  </DefaultLayout>
-  )
-}
-  
-
-
-Index.getInitialProps = async () => {
-  // Here we call the API and request 5 documents
-  const response = await getData({ pageSize: 20});
-  return {
-    downloads: response.results
+  handleSubmit = e => {
+    e.preventDefault();
+   
+    if( e.target.email.value === "suporte@indexatecnologia.com.br" &&
+    e.target.password.value === "depechemode"){
+        window.location.href = "/download"
+    }else{
+        alert("Email ou senha incorretos");
+    }
+    if (!e.target.email.value) {
+      alert("Email não digitado");
+    } else if (!e.target.email.value) {
+      alert("Email invalido");
+    } else if (!e.target.password.value) {
+      alert("Senha é obrigatorio");
+    } 
   };
-};
 
-export default Index;
+  render() {
+    return (
+        
+<div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10 mx-auto mt-40">
+    <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
+       IDX SUPORTE
+    </div>
+    <div className="mt-8">
+        <form action="#" autoComplete="off" onSubmit={this.handleSubmit}>
+            <div className="flex flex-col mb-2">
+                <div className="flex relative ">
+                    <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                        <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z">
+                            </path>
+                        </svg>
+                    </span>
+                    <input type="email" id="sign-in-email" name="email" className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange focus:border-transparent" placeholder="Email"/>
+                    </div>
+                </div>
+                <div className="flex flex-col mb-6">
+                    <div className="flex relative ">
+                        <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z">
+                                </path>
+                            </svg>
+                        </span>
+                        <input type="password" id="sign-in-email" name="password" className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange focus:border-transparent" placeholder="Senha"/>
+                        </div>
+                    </div>
+                    
+                    <div className="flex w-full">
+                        <button type="submit" className="py-2 px-4  bg-orange hover:bg-orangeHover focus:ring-orange focus:ring-offset-orange text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+
+      
+    );
+  }
+}
+
+export default Home;
